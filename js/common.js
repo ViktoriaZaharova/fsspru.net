17fc7fadf4691ec054fdaeddeefb23e267128b54
Виктория Захарова <zaharova181114@gmail.com>
2023-09-09 00:03:51 +0300

    + 2 pages

--- js/common.js
+++ js/common.js
@@ -27,3 +27,22 @@ $('.toggle-regions').on('click', function (e) {
 $('.input-dropdown').on('click', function () {
 	$(this).next('.dropdown-menu').toggleClass('show');
 });
+
+// input search
+$('.home-form input').on('keyup change', function () {
+	if (this.value.length > 0) {
+		$(this).parent('.form-group').find('.dropdown-menu-result').addClass('show');
+	} else {
+		$('.dropdown-menu-result').removeClass('show');
+	}
+});
+
+// активная ссылка меню
+$('.wrapperLinksHeader li .nav-link').each(function () {
+	let location = window.location.href;
+	let link = this.href;
+	if (location === link) {
+		$(this).addClass('active');
+	}
+});
+// end
