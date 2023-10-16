6addb58b0e7ed5cfe0f8f3789b84bb0b7d2bb35f
Виктория Захарова <zaharova181114@gmail.com>
2023-10-16 15:15:46 +0300

    edit1

--- js/common.js
+++ js/common.js
@@ -1,4 +1,3 @@
-// $('[name="phone"]').mask('+7 (999) 999-99-99');
 $('.tibrr-cookie-consent-button').on('click', function (e) {
 	e.preventDefault();
 	$('.tibrr-cookie-consent').fadeOut();
@@ -24,8 +23,8 @@ $('.toggle-regions').on('click', function (e) {
 	}
 });
 
-$('.input-dropdown').on('click', function () {
-	$(this).next('.dropdown-menu').toggleClass('show');
+$('.dropdown-regions').on('click', function () {
+	$(this).find('.dropdown-menu').toggleClass('show');
 });
 
 // input search
@@ -81,6 +80,95 @@ $('.departmens-box__toggle').on('click', function (e) {
 });
 // show list all
 
+// tabs dropdown mobile
+const $tabsToDropdown = $(".tabs-to-dropdown");
+
+function generateDropdownMarkup(container) {
+	const $navWrapper = container.find(".nav-wrapper");
+	const $navPills = container.find(".nav-pills");
+	const firstTextLink = $navPills.find("li:first-child a").text();
+	const $items = $navPills.find("li");
+	const markup = `
+    <div class="dropdown d-sm-none">
+		<span class="input-title">
+														Поиск по:
+													</span>
+      <button class="btn dropdown-toggle dropdown-toggle-tab" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        ${firstTextLink}
+      </button>
+      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton"> 
+        ${generateDropdownLinksMarkup($items)}
+      </div>
+    </div>
+  `;
+	$navWrapper.prepend(markup);
+}
+
+function generateDropdownLinksMarkup(items) {
+	let markup = "";
+	items.each(function () {
+		const textLink = $(this).find("a").text();
+		markup += `<a class="dropdown-item" href="#">${textLink}</a>`;
+	});
+
+	return markup;
+}
+
+function showDropdownHandler(e) {
+	// works also
+	//const $this = $(this);
+	const $this = $(e.target);
+	const $dropdownToggle = $this.find(".dropdown-toggle");
+	const dropdownToggleText = $dropdownToggle.text().trim();
+	const $dropdownMenuLinks = $this.find(".dropdown-menu a");
+	const dNoneClass = "d-none";
+	$dropdownMenuLinks.each(function () {
+		const $this = $(this);
+		if ($this.text() == dropdownToggleText) {
+			$this.addClass(dNoneClass);
+		} else {
+			$this.removeClass(dNoneClass);
+		}
+	});
+}
+
+function clickHandler(e) {
+	e.preventDefault();
+	const $this = $(this);
+	const index = $this.index();
+	const text = $this.text();
+	$this.closest(".dropdown").find(".dropdown-toggle").text(`${text}`);
+	$this
+		.closest($tabsToDropdown)
+		.find(`.nav-pills li:eq(${index}) a`)
+		.tab("show");
+}
+
+function shownTabsHandler(e) {
+	// works also
+	//const $this = $(this);
+	const $this = $(e.target);
+	const index = $this.parent().index();
+	const $parent = $this.closest($tabsToDropdown);
+	const $targetDropdownLink = $parent.find(".dropdown-menu a").eq(index);
+	const targetDropdownLinkText = $targetDropdownLink.text();
+	$parent.find(".dropdown-toggle").text(targetDropdownLinkText);
+}
+
+$tabsToDropdown.each(function () {
+	const $this = $(this);
+	const $pills = $this.find('a[data-toggle="pill"]');
+
+	generateDropdownMarkup($this);
+
+	const $dropdown = $this.find(".dropdown");
+	const $dropdownLinks = $this.find(".dropdown-menu a");
+
+	$dropdown.on("show.bs.dropdown", showDropdownHandler);
+	$dropdownLinks.on("click", clickHandler);
+	$pills.on("shown.bs.tab", shownTabsHandler);
+});
+
 
 $(".bailiff-next-step").click(function (e) {
 	e.preventDefault();
@@ -118,16 +206,57 @@ $('#additionalOrder').on('click', function () {
 
 	if (!$this.hasClass('trigger')) {
 		$this.addClass('trigger');
-		$this.find('span').html('Убрать заявителя ');
+		$this.find('span').html('Убрать заявителя');
 		$('.form-bailiff__price').slideUp();
 		content.slideDown();
+		$(this).parents('.form-bailiff').find('.btn-report-sum').addClass('sale-price');
 	} else {
 		$this.removeClass('trigger');
 		$this.find('span').html('Добавить заявителя за 99р');
 		$('.form-bailiff__price').slideDown();
+		$(this).parents('.form-bailiff').find('.btn-report-sum').removeClass('sale-price');
 		content.slideUp();
 	}
 });
+
+$('.link-code').on('click', function (e) {
+	e.preventDefault();
+	var
+		$this = $(this),
+		content1 = $(this).parents('label').siblings('[name="email"]'),
+		content2 = $(this).parents('label').siblings('[name="phone"]');
+
+	if (!$this.hasClass('click')) {
+		$this.addClass('click');
+		$this.html('Отправить в СМС');
+		content1.attr('hidden', true);
+		content2.attr('hidden', false);
+	} else {
+		$this.removeClass('click');
+		$this.html('Укажите телефон');
+		content1.attr('hidden', false);
+		content2.attr('hidden', true);
+	}
+});
+
+$('[name="phone"]').mask('+7(999)999-99-99');
+
+$.fn.setCursorPosition = function (pos) {
+	if ($(this).get(0).setSelectionRange) {
+		$(this).get(0).setSelectionRange(pos, pos);
+	} else if ($(this).get(0).createTextRange) {
+		var range = $(this).get(0).createTextRange();
+		range.collapse(true);
+		range.moveEnd('character', pos);
+		range.moveStart('character', pos);
+		range.select();
+	}
+};
+
+$('[name="phone"]').click(function () {
+	$(this).setCursorPosition(3);
+}).mask("+7(999)999-9999");
+$('[name="phone"]').mask("+7(999)999-9999");
 
 // show fixed block added payment
 $('.add-payment').on('click', function (e) {
@@ -151,9 +280,9 @@ $('.add-payment').on('click', function (e) {
 // if input = checked - box show
 $('.form-check-label-payment input:checkbox').change(function () {
 	if ($(this).is(":checked")) {
-		$(this).parents('.departmens-box').find('.departmens-box-item-checked').addClass('click');
+		$(this).parents('.departmens-box').addClass('click');
 	} else {
-		$(this).parents('.departmens-box').find('.departmens-box-item-checked').removeClass('click');
+		$(this).parents('.departmens-box').removeClass('click');
 	}
 });
 
@@ -163,6 +292,16 @@ $('.departmens-box-form input').on('keyup change', function () {
 	} else {
 		$(this).parents('.departmens-box-form').removeClass('checked');
 	}
+});
+
+$('.checked-form').on('click', function (e) {
+	e.preventDefault;
+	$(this).parents('.departmens-box').addClass('active');
+});
+
+$('.sum-value-edit').on('click', function (e) {
+	e.preventDefault;
+	$(this).parents('.departmens-box').removeClass('active');
 });
 
 // animate scroll
